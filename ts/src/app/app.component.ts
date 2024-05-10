8bbe5aa9186ab27c9ffb51b2953af98f7e0159ed
miqueiasmartinsf <126704294+miqueiasmartinsf@users.noreply.github.com>
2024-05-10 14:10:41 -0300

    feat: add onClick function in AppComponent class

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DevChuva';
+  
+    onClick(event:any):void{
+        const element = event.target;
+        element.classList.add = "selected";
+    }
 }
